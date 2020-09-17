13e40546908a673b9bf127823fabb9a23ca611c0
zongzhang <jingchang_007@163.com>
2020-09-17 14:50:52 +0800

    修改state例子

--- src/index.js
+++ src/index.js
@@ -1,7 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+//不要直接修改 State
+//State 的更新可能是异步的
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
 
-const element = <h1>Hello, world</h1>;
+    //生命周期方法
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
 
-ReactDOM.render(element, document.getElementById('root'));
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        )
+    }
+}
+
+function tick() {
+    ReactDOM.render(
+        <Clock/>,
+        document.getElementById('root')
+    );
+}
+
+setInterval(tick, 1000);
