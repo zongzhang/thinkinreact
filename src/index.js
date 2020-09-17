d10feca3726e0c4c3a6dbed0949353276f74ccd8
zongzhang <jingchang_007@163.com>
2020-09-17 11:28:11 +0800

    修改组件例子

--- src/index.js
+++ src/index.js
@@ -2,6 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const element = <h1>Hello, world</h1>;
+//函数组件
+// function Welcome(props) {
+//     return <h1>Hello, {props.name}</h1>;
+// }
 
-ReactDOM.render(element, document.getElementById('root'));
+class Welcome extends React.Component {
+    render() {
+        return <h1>Hello, {this.props.name}</h1>;
+    }
+}
+
+// 元素是用户自定义组件
+const element = <Welcome name="Sara"/>;
+
+function App() {
+    return (
+        <div>
+            <Welcome name="Sara"/>
+            <Welcome name="Cahal"/>
+            <Welcome name="Edite"/>
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
