b9f66d1c29a6f0b12ba2bff1b46e3b7df8be1981
zongzhang <jingchang_007@163.com>
2020-09-17 10:50:37 +0800

    修改hello例子

--- src/index.js
+++ src/index.js
@@ -2,19 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const user = {
-    firstName: 'Hello',
-    lastName: 'World',
-}
+const element = <h1>Hello, world</h1>;
 
-function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
-}
-
-const element = (
-    <h1>haha, {formatName(user)}</h1>
-)
-ReactDOM.render(
-    element,
-    document.getElementById('root')
-);
+ReactDOM.render(element, document.getElementById('root'));
