5930c8c9c1475023a49450bed36053d60b0121e0
zongzhang <jingchang_007@163.com>
2020-09-17 10:13:19 +0800

    helloworld添加

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+const user = {
+    firstName: 'harper',
+    lastName: 'perez',
+}
+
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+const element = (
+    <h1>Hello, {formatName(user)}</h1>
+)
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    element,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
