8a22b75d25f30821182b4567d083c74c4780e283
zongzhang <jingchang_007@163.com>
2020-09-18 13:33:10 +0800

    修改条件渲染例子

--- src/index.js
+++ src/index.js
@@ -2,6 +2,78 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const element = <h1>Hello, world</h1>;
+function UserGreeting(props) {
+    return <h1>Welcome back!</h1>;
+}
 
-ReactDOM.render(element, document.getElementById('root'));
+function GuestGreeting(props) {
+    return <h1>Please sign up.</h1>;
+}
+
+function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) {
+        return <UserGreeting/>;
+    }
+    return <GuestGreeting/>;
+}
+
+function LoginButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Login
+        </button>
+    );
+}
+
+function LogoutButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Logout
+        </button>
+    );
+}
+// 有状态的组件
+class LoginControl extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleLoginClick = this.handleLoginClick.bind(this);
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
+        this.state = {isLoggedIn: false};
+    }
+
+    handleLoginClick() {
+        this.setState({isLoggedIn: true});
+    }
+
+    handleLogoutClick() {
+        this.setState({isLoggedIn: false});
+    }
+
+    render() {
+        const isLoggedIn = this.state.isLoggedIn;
+        let button;
+        if (isLoggedIn) {
+            button = <LogoutButton onClick={this.handleLogoutClick}/>;
+        } else {
+            button = <LoginButton onClick={this.handleLoginClick}/>;
+        }
+
+        return (
+            <div>
+                <Greeting isLoggedIn={isLoggedIn}/>
+                {button}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<LoginControl/>, document.getElementById('root'));
+
+// true && expression 总是会返回 expression, 而 false && expression 总是会返回 false。
+// 因此，如果条件是 true，&& 右侧的元素就会被渲染，如果是 false，React 会忽略并跳过它。
+// {unreadMessages.length > 0 &&
+// <h2>
+//     You have {unreadMessages.length} unread messages.
+// </h2>
+// }
