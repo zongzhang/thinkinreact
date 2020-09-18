2670bdd054959fe60f4590b0243065c43fc441fd
zongzhang <jingchang_007@163.com>
2020-09-18 13:41:09 +0800

    修改list及key例子

--- src/index.js
+++ src/index.js
@@ -2,6 +2,78 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const element = <h1>Hello, world</h1>;
+//key 帮助 React 识别哪些元素改变了，比如被添加或删除。因此你应当给数组中的每一个元素赋予一个确定的标识。
 
-ReactDOM.render(element, document.getElementById('root'));
+// function NumberList(props) {
+//     const numbers = props.numbers;
+//     const listItems = numbers.map((number) =>
+//         <li key={number.toString()}>{number}</li>
+//     );
+//     return (
+//         <ul>{listItems}</ul>
+//     );
+// }
+//
+// const numbers = [1, 2, 3, 4, 5];
+// ReactDOM.render(
+//     <NumberList numbers={numbers} />,
+//     document.getElementById('root')
+// );
+//
+// function ListItem(props) {
+//     // 正确！这里不需要指定 key：
+//     return <li>{props.value}</li>;
+// }
+//
+// function NumberList(props) {
+//     const numbers = props.numbers;
+//     const listItems = numbers.map((number) =>
+//         // 正确！key 应该在数组的上下文中被指定
+//         <ListItem key={number.toString()} value={number} />
+//     );
+//     return (
+//         <ul>
+//             {listItems}
+//         </ul>
+//     );
+// }
+//
+// const numbers = [1, 2, 3, 4, 5];
+// ReactDOM.render(
+//     <NumberList numbers={numbers} />,
+//     document.getElementById('root')
+// );
+
+function Blog(props) {
+    const sidebar = (
+        <ul>
+            {props.posts.map((post) =>
+                <li key={post.id}>
+                    {post.title}
+                </li>
+            )}
+        </ul>
+    );
+    const content = props.posts.map((post) =>
+        <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.content}</p>
+        </div>
+    );
+    return (
+        <div>
+            {sidebar}
+            <hr />
+            {content}
+        </div>
+    );
+}
+
+const posts = [
+    {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
+    {id: 2, title: 'Installation', content: 'You can install React from npm.'}
+];
+ReactDOM.render(
+    <Blog posts={posts} />,
+    document.getElementById('root')
+);
