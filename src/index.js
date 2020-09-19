0e879c2b127efe3f3377c07b25bff709b30edd83
zongzhang <jingchang_007@163.com>
2020-09-19 22:49:10 +0800

    状态提升

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function BoilingVerdict(props) {
+    if (props.celsius >= 100) {
+        return <p>The water would boil.</p>;
+    }
+    return <p>The water would not boil.</p>;
+}
+
+
 const element = <h1>Hello, world</h1>;
 
 ReactDOM.render(element, document.getElementById('root'));
