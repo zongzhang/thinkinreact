fc6f6bd1e5240c3e8e00296eb77aa4de28491140
zongzhang <jingchang_007@163.com>
2020-09-19 23:51:40 +0800

    状态提升

--- src/index.js
+++ src/index.js
@@ -10,6 +10,60 @@ function BoilingVerdict(props) {
 }
 
 
-const element = <h1>Hello, world</h1>;
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {temperature: ''};
+    }
 
-ReactDOM.render(element, document.getElementById('root'));
+    handleChange(e) {
+        this.setState({temperature: e.target.value});
+    }
+
+    render() {
+        const temperature = this.state.temperature;
+        return (
+            <fieldset>
+                <legend>Enter temperature in Celsius:</legend>
+                <input
+                    value={temperature}
+                    onChange={this.handleChange}/>
+                <BoilingVerdict
+                    celsius={parseFloat(temperature)}/>
+            </fieldset>
+        )
+    }
+}
+
+const scaleNames = {
+    c: 'Celsius',
+    f: 'Fahrenheit'
+};
+
+class TemperatureInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {temperature: ''};
+    }
+
+    handleChange(e) {
+        this.setState({temperature: e.target.value});
+    }
+
+    render() {
+        const temperature = this.state.temperature;
+        const scale = this.props.scale;
+        return (
+            <fieldset>
+                <legend>Enter temperature in {scaleNames[scale]}:</legend>
+                <input value={temperature}
+                       onChange={this.handleChange} />
+            </fieldset>
+        );
+    }
+}
+
+
+ReactDOM.render(<Calculator/>, document.getElementById('root'));
